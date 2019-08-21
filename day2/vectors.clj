5c5b135349f43d7b7d9008d2f4fb956d8e8409bd
João Paulo Moraes <joaopaulomoraes@outlook.com>
2019-08-20 22:57:05 -0300

    feat: Added vectors example

---
+++ day2/vectors.clj
@@ -0,0 +1,19 @@
+(ns seven-days-of-clojure.vectors)
+
+(conj [0 9 8 7 6 5] 4)
+
+(conj [:a :b "c"] 5)
+
+;; nth
+(nth [0 9 8 7 6 5] 4)
+
+(first [:a :b "c"])
+
+(last [0 1 1 3 "I'm last"])
+
+;; nth first last
+(nth [0 9 8 7 6 5] 4)
+
+(first [:a :b "c"])
+
+(last [0 1 1 3 "I'm last"])
