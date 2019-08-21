4ebf87244b3b074d3a1ddf533a87f2c73b37b8ae
João Paulo Moraes <joaopaulomoraes@outlook.com>
2019-08-20 21:37:57 -0300

    feat: Added string examples

---
+++ day2/strings.clj
@@ -0,0 +1,13 @@
+(ns seven-days-of-clojure.strings)
+
+(str "I'm " "new!")
+
+(format "Welcome to %s" "Clojure!")
+
+(format "This is the %dnd day of %s" 2 "Clojure")
+
+(format "Φ: %.2f" 1.61803398875)
+
+(format "Φ: %.7f" 1.61803398875)
+
+(format "if (= 2 (+ 1 1)) returns %b" true)
