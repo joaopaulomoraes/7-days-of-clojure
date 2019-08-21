34d8d491208c6bc09b58bf211cf28f0214d3060b
João Paulo Moraes <joaopaulomoraes@outlook.com>
2019-08-20 23:06:50 -0300

    fix: Remove parentheses from maps

--- day2/maps.clj
+++ day2/maps.clj
@@ -1,17 +1,17 @@
 (ns seven-days-of-clojure.maps)
 
-({})
+{}
 
-({:id "abcd-1234"
+{:id "abcd-1234"
  :title "My Map"
  :color "aqua"
  :author {:id "dcba-4321"
-          :name "@me"}})
+          :name "@me"}}
 
-({:language "Clojure"
+{:language "Clojure"
  :version "1.10.0"
  :author "Rich Hickey"
- :year 2007})
+ :year 2007}
 
  (get {:id "abcd-1234"
   :title "My Map"
