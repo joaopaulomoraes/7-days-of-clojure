3c980c364562dfbe6822a117ad37fe262110e12e
João Paulo Moraes <joaopaulomoraes@outlook.com>
2019-08-20 22:57:10 -0300

    feat: Added maps example

---
+++ day2/maps.clj
@@ -0,0 +1,57 @@
+(ns seven-days-of-clojure.maps)
+
+({})
+
+({:id "abcd-1234"
+ :title "My Map"
+ :color "aqua"
+ :author {:id "dcba-4321"
+          :name "@me"}})
+
+({:language "Clojure"
+ :version "1.10.0"
+ :author "Rich Hickey"
+ :year 2007})
+
+ (get {:id "abcd-1234"
+  :title "My Map"
+  :color "aqua"
+  :author {:id "dcba-4321"
+            :name "@me"}} :color)
+
+;; get
+(get {:language "Clojure"
+  :version "1.10.0"
+  :author "Rich Hickey"
+  :year 2007} :language)
+
+;; assoc
+(assoc {:language "Clojure"
+  :version "1.10.0"
+  :author "Rich Hickey"
+  :year 2007} :dialect "Lisp")
+
+(assoc {:id "abcd-1234"
+  :title "My Map"
+  :color "aqua"
+  :author {:id "dcba-4321"
+            :name "@me"}} :title "My new Map")
+
+;; merge
+(merge {:a "a"} {:b "b"})
+
+(merge {:id "0123-defg"
+        :name "Map"} {:keys {:id :name}})
+
+;; keys
+(keys {:id "abcd-1234"
+  :title "My Map"
+  :color "aqua"
+  :author {:id "dcba-4321"
+           :name "@me"}})
+
+;; vals
+(vals {:language "Clojure"
+  :version "1.10.0"
+  :author "Rich Hickey"
+  :year 2007})
