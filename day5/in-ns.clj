478439491f9b7b2f0efad0ccbb997c5965b56de3
João Paulo Moraes <joaopaulomoraes@outlook.com>
2019-08-24 00:40:53 -0300

    feat: Added in-ns example

---
+++ day5/in-ns.clj
@@ -0,0 +1,10 @@
+(ns seven-days-of-clojure.in-ns)
+
+(in-ns 'test)
+;; user=> #object[clojure.lang.Namespace 0x415156bf "test"]
+
+(in-ns 'calculate)
+;; user=> #object[clojure.lang.Namespace 0x1ecfcbc9 "calculate"]
+
+(in-ns 'some-other)
+;; user=> #object[clojure.lang.Namespace 0x660591fb "some-ohter"]
