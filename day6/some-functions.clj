0cee0251273cca3e26061b80763ea576557dc23f
João Paulo Moraes <joaopaulomoraes@outlook.com>
2019-08-25 00:46:58 -0300

    feat: Added some-functions example

---
+++ day6/some-functions.clj
@@ -0,0 +1,34 @@
+(ns seven-days-of-clojure.some-functions)
+
+;; number
+(defn one
+  []
+    1)
+
+;; floating-point
+(defn floating-point
+  []
+    7.7)
+
+;; String
+(defn language
+  []
+    (str "Clojure"))
+
+;; boolean
+(defn is-bool
+  []
+    true)
+
+;; map
+(defn author
+  []
+    {:id "id-01"
+     :name "author"
+     :email "author@clj.map"})
+
+;; keyword
+(defn paradigm
+  []
+    :fp)
+  
